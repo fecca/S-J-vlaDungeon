0a80096eadb054560cb064b48d451643332bfd75
fecca <niklas.cronfalk@gmail.com>
2016-10-18 00:34:31 +0200

    ToDo + Node script

---
+++ Assets/Scripts/PathFinding/PathfindingNode.cs
@@ -0,0 +1,28 @@
+﻿public class PathfindingNode
+{
+	public float X;
+	public float Y;
+	public bool Walkable;
+	public PathfindingNode Parent;
+
+	public float GCost { get; set; }
+	public float HCost { get; set; }
+	public float FCost { get; set; }
+
+	public PathfindingNode(float x, float y, bool walkable)
+	{
+		X = x;
+		Y = y;
+		Walkable = walkable;
+	}
+
+	public override bool Equals(object other)
+	{
+		var otherNode = (PathfindingNode)other;
+		return X == otherNode.X && Y == otherNode.Y;
+	}
+	public override string ToString()
+	{
+		return X + ";" + Y;
+	}
+}
